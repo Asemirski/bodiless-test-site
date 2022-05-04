ce11ad2563eed17e8f28a97bccb229adddd74910
Aliaksei Semirski <aliaksei_semirski@epam.com>
2022-05-04 13:51:53 +0200

    fail line removed

--- edit/platform.custom.sh
+++ edit/platform.custom.sh
@@ -14,7 +14,5 @@
  ##
 finalize_deploy () {
   npm run bootstrap -- --ci
-  npm run build:packages -- --concurrency 1
-  npm run build:env-vars -- --scope=@sites/${APP_SITE_NAME}
-  npm run build:doc -- --scope=@sites/${APP_SITE_NAME}
+  npm run build
 }
