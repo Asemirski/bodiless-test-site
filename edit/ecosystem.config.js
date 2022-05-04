951af6d0e47a9390d611a1b35c2acae75cdeb53c
Chris Oden <coden@its.jnj.com>
2022-05-04 07:16:34 -0400

    pm2 debug info

--- edit/ecosystem.config.js
+++ edit/ecosystem.config.js
@@ -3,25 +3,22 @@ const fs = require('fs');
 const appVolume = () => process.env.APP_VOLUME || '/app/volume';
 const gatsbyPort = () => process.env.PORT || 8888;
 const siteDirName = () => process.env.APP_SITE_DIR_NAME || 'test-site';
-const backend = () => {
-  const backendRelative = 'node_modules/@bodiless/backend/src/server.js';
-  const siteLevelBackend = `${appVolume()}/root/sites/${siteDirName()}/${backendRelative}`;
-  return fs.existsSync(siteLevelBackend)
-    ? siteLevelBackend : `${appVolume()}/root/${backendRelative}`;
-};
+const cwd = `${appVolume()}/root/sites/${siteDirName()}`;
+const feScript = `${appVolume()}/root/node_modules/.bin/gatsby`;
+const beScript = `${appVolume()}/root/node_modules/.bin/bodiless-backend`;
 
 module.exports = {
   apps: [
     {
       name: 'frontend',
-      cwd: `${appVolume()}/root/sites/${siteDirName()}`,
-      script: `${appVolume()}/root/node_modules/.bin/gatsby`,
+      cwd,
+      script: feScript,
       args: `develop --port ${gatsbyPort()}`,
     },
     {
       name: 'backend',
-      cwd: `${appVolume()}/root/sites/${siteDirName()}`,
-      script: backend(),
+      cwd,
+      script: beScript,
     },
   ],
 };
