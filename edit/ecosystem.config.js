cab2a728ad582036eaed7e9b63ea0b1a6433f1d5
Chris Oden <coden@its.jnj.com>
2022-05-04 07:18:05 -0400

    Add print

--- edit/ecosystem.config.js
+++ edit/ecosystem.config.js
@@ -6,6 +6,7 @@ const siteDirName = () => process.env.APP_SITE_DIR_NAME || 'test-site';
 const cwd = `${appVolume()}/root/sites/${siteDirName()}`;
 const feScript = `${appVolume()}/root/node_modules/.bin/gatsby`;
 const beScript = `${appVolume()}/root/node_modules/.bin/bodiless-backend`;
+console.log(cwd, feScript, beScript);
 
 module.exports = {
   apps: [
